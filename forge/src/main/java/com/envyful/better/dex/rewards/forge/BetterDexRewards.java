b3fcd51ad273618346e1546af05edae5b4f4735e
danorris709 <dnorris443@gmail.com>
2021-09-06 14:31:59 +0100

    feat(forge): load config on start

--- forge/src/main/java/com/envyful/better/dex/rewards/forge/BetterDexRewards.java
+++ forge/src/main/java/com/envyful/better/dex/rewards/forge/BetterDexRewards.java
@@ -1,9 +1,13 @@
 package com.envyful.better.dex.rewards.forge;
 
+import com.envyful.api.config.yaml.YamlConfigFactory;
 import com.envyful.api.forge.command.ForgeCommandFactory;
+import com.envyful.better.dex.rewards.forge.config.BetterDexRewardsConfig;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+
+import java.io.IOException;
 
 @Mod(
         modid = "betterdexrewards",
@@ -19,13 +23,21 @@ public class BetterDexRewards {
 
     private ForgeCommandFactory commandFactory = new ForgeCommandFactory();
 
+    private BetterDexRewardsConfig config;
+
     @Mod.EventHandler
     public void onInit(FMLInitializationEvent event) {
         instance = this;
+
+        this.reloadConfig();
     }
 
     public void reloadConfig() {
-
+        try {
+            this.config = YamlConfigFactory.getInstance(BetterDexRewardsConfig.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Mod.EventHandler
