a936be758f23c4faeb8dde5fec08698ac293ed39
danorris709 <dnorris443@gmail.com>
2021-09-06 14:48:52 +0100

    feat(forge): load database & create table

--- forge/src/main/java/com/envyful/better/dex/rewards/forge/BetterDexRewards.java
+++ forge/src/main/java/com/envyful/better/dex/rewards/forge/BetterDexRewards.java
@@ -1,15 +1,22 @@
 package com.envyful.better.dex.rewards.forge;
 
+import com.envyful.api.concurrency.UtilConcurrency;
 import com.envyful.api.config.yaml.YamlConfigFactory;
+import com.envyful.api.database.Database;
+import com.envyful.api.database.impl.SimpleHikariDatabase;
 import com.envyful.api.forge.command.ForgeCommandFactory;
 import com.envyful.api.forge.player.ForgePlayerManager;
 import com.envyful.better.dex.rewards.forge.config.BetterDexRewardsConfig;
+import com.envyful.better.dex.rewards.forge.config.BetterDexRewardsQueries;
 import com.envyful.better.dex.rewards.forge.player.DexRewardsAttribute;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @Mod(
         modid = "betterdexrewards",
@@ -26,6 +33,7 @@ public class BetterDexRewards {
     private ForgeCommandFactory commandFactory = new ForgeCommandFactory();
     private ForgePlayerManager playerManager = new ForgePlayerManager();
 
+    private Database database;
     private BetterDexRewardsConfig config;
 
     @Mod.EventHandler
@@ -35,6 +43,17 @@ public class BetterDexRewards {
         this.reloadConfig();
 
         this.playerManager.registerAttribute(this, DexRewardsAttribute.class);
+
+        UtilConcurrency.runAsync(() -> {
+            this.database = new SimpleHikariDatabase(this.config.getDatabase());
+
+            try (Connection connection = this.database.getConnection();
+                 PreparedStatement preparedStatement = connection.prepareStatement(BetterDexRewardsQueries.CREATE_TABLE)) {
+                preparedStatement.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     public void reloadConfig() {
@@ -51,5 +70,13 @@ public class BetterDexRewards {
 
     public static BetterDexRewards getInstance() {
         return instance;
+    }
+
+    public ForgePlayerManager getPlayerManager() {
+        return this.playerManager;
+    }
+
+    public Database getDatabase() {
+        return this.database;
     }
 }
