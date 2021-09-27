4fb409ec0cc4fc74ae204e9be95e3c98fa62293e
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-09-27 12:11:16 +0100

    feat(forge): implement moveable config items

--- forge/src/main/java/com/envyful/better/dex/rewards/forge/config/BetterDexRewardsConfig.java
+++ forge/src/main/java/com/envyful/better/dex/rewards/forge/config/BetterDexRewardsConfig.java
@@ -3,13 +3,21 @@ package com.envyful.better.dex.rewards.forge.config;
 import com.envyful.api.config.data.ConfigPath;
 import com.envyful.api.config.type.ConfigInterface;
 import com.envyful.api.config.type.ConfigItem;
+import com.envyful.api.config.type.PositionableConfigItem;
 import com.envyful.api.config.type.SQLDatabaseDetails;
 import com.envyful.api.config.yaml.AbstractYamlConfig;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.pixelmonmod.pixelmon.config.PixelmonItems;
+import com.pixelmonmod.pixelmon.config.PixelmonItemsPokeballs;
+import com.pixelmonmod.pixelmon.enums.EnumSpecies;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 
+import javax.swing.text.Position;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -18,18 +26,50 @@ import java.util.Map;
 public class BetterDexRewardsConfig extends AbstractYamlConfig {
 
     private SQLDatabaseDetails database = new SQLDatabaseDetails("BetterDexRewards", "0.0.0.0", 3306,
-            "admin", "password", "BetterDexRewards");
+                                                                 "admin", "password", "BetterDexRewards"
+    );
 
     private ConfigInterface configInterface = new ConfigInterface();
 
-    private ConfigItem infoItem = new ConfigItem();
+    private PositionableConfigItem infoItem = new PositionableConfigItem(
+            Item.getIdFromItem(Items.PAPER) + "",
+            1, (byte) 0, "Info",
+            Lists.newArrayList(""),
+            7, 1,
+            Collections.emptyMap()
+    );
+
+    private PositionableConfigItem missingItem = new PositionableConfigItem(
+            Item.getIdFromItem(PixelmonItems.itemPixelmonSprite) + "",
+            1, (byte) 0, "&eMissing Pokemon",
+            Lists.newArrayList(""),
+            5, 1,
+            ImmutableMap.of("ndex", new ConfigItem.NBTValue("byte", EnumSpecies.Unown.getNationalPokedexInteger() + ""))
+    );
+
+    private PositionableConfigItem ranksItem = new PositionableConfigItem(
+            Item.getIdFromItem(PixelmonItems.itemFinder) + "",
+            1, (byte) 0, "&ePokeDex Ranks",
+            Lists.newArrayList(""),
+            3, 1,
+            Collections.emptyMap()
+    );
+
+    private PositionableConfigItem percentageItem = new PositionableConfigItem(
+            Item.getIdFromItem(PixelmonItemsPokeballs.pokeBall) + "",
+            1, (byte) 0, "&eCurrent PokeDex Percentage",
+            Lists.newArrayList("&eComplete: &a%percentage%"),
+            1, 1,
+            ImmutableMap.of("tooltip", new ConfigItem.NBTValue("string", ""))
+    );
 
     private int messageDelaySeconds = 60;
 
     private Map<String, DexCompletion> rewardStages = Maps.newHashMap(ImmutableMap.of(
             "one", new DexCompletion(1, 1, new ConfigItem(), new ConfigItem(), new ConfigItem(), 1.0,
-                    Lists.newArrayList("give %player% minecraft:diamond 1"),
-                    Lists.newArrayList("&e&l(!) &eYou have completed 1% of the dex!"))
+                                     Lists.newArrayList("give %player% minecraft:diamond 1"),
+                                     Lists.newArrayList("&e&l(!) &eYou have completed 1% of the dex!")
+            )
     ));
 
     private List<String> claimReminderMessage = Lists.newArrayList(
@@ -43,7 +83,7 @@ public class BetterDexRewardsConfig extends AbstractYamlConfig {
     public BetterDexRewardsConfig() {
     }
 
-    public ConfigItem getInfoItem() {
+    public PositionableConfigItem getInfoItem() {
         return this.infoItem;
     }
 
@@ -69,6 +109,18 @@ public class BetterDexRewardsConfig extends AbstractYamlConfig {
 
     public int getMessageDelaySeconds() {
         return this.messageDelaySeconds;
+    }
+
+    public PositionableConfigItem getMissingItem() {
+        return this.missingItem;
+    }
+
+    public PositionableConfigItem getRanksItem() {
+        return this.ranksItem;
+    }
+
+    public PositionableConfigItem getPercentageItem() {
+        return this.percentageItem;
     }
 
     @ConfigSerializable
--- forge/src/main/java/com/envyful/better/dex/rewards/forge/ui/DexRewardsMainUI.java
+++ forge/src/main/java/com/envyful/better/dex/rewards/forge/ui/DexRewardsMainUI.java
@@ -2,22 +2,23 @@ package com.envyful.better.dex.rewards.forge.ui;
 
 import com.envyful.api.config.type.ConfigInterface;
 import com.envyful.api.config.type.ConfigItem;
+import com.envyful.api.config.type.PositionableConfigItem;
 import com.envyful.api.forge.chat.UtilChatColour;
+import com.envyful.api.forge.config.UtilConfigItem;
 import com.envyful.api.forge.items.ItemBuilder;
 import com.envyful.api.gui.factory.GuiFactory;
 import com.envyful.api.gui.pane.Pane;
 import com.envyful.api.player.EnvyPlayer;
 import com.envyful.better.dex.rewards.forge.BetterDexRewards;
+import com.envyful.better.dex.rewards.forge.config.BetterDexRewardsConfig;
 import com.envyful.better.dex.rewards.forge.player.DexRewardsAttribute;
-import com.pixelmonmod.pixelmon.config.PixelmonItems;
-import com.pixelmonmod.pixelmon.config.PixelmonItemsPokeballs;
-import com.pixelmonmod.pixelmon.enums.EnumSpecies;
+import com.google.common.collect.Lists;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagShort;
-import net.minecraft.nbt.NBTTagString;
 import net.minecraftforge.fml.common.FMLCommonHandler;
+
+import java.util.List;
 
 public class DexRewardsMainUI {
 
@@ -55,60 +56,51 @@ public class DexRewardsMainUI {
         }
 
         double percentage = attribute.getPokeDexPercentage();
+        BetterDexRewardsConfig actualConfig = BetterDexRewards.getInstance().getConfig();
 
-        pane.set(1, 1, GuiFactory.displayableBuilder(ItemStack.class)
-                .itemStack(new ItemBuilder()
-                        .type(PixelmonItemsPokeballs.pokeBall)
-                        .amount(1)
-                        .name(UtilChatColour.translateColourCodes('&', "&eCurrent PokeDex Percentage"))
-                        .lore(
-                                UtilChatColour.translateColourCodes('&',
-                                                "&eComplete: &a" + String.format("%.2f", percentage) + "%")
-                        )
-                        .nbt("tooltip", new NBTTagString(""))
-                        .build())
+        pane.set(actualConfig.getPercentageItem().getXPos(), actualConfig.getPercentageItem().getYPos(),
+                 GuiFactory.displayableBuilder(ItemStack.class)
+                .itemStack(new ItemBuilder(UtilConfigItem.fromConfigItem(actualConfig.getPercentageItem()))
+                        .lore(getLore(percentage, actualConfig.getPercentageItem())).build())
                 .build());
 
-        pane.set(3, 1, GuiFactory.displayableBuilder(ItemStack.class)
-                .itemStack(new ItemBuilder()
-                        .type(PixelmonItems.itemFinder)
-                        .amount(1)
-                        .name(UtilChatColour.translateColourCodes('&', "&ePokeDex Ranks"))
-                        .lore()
-                        .build())
+        pane.set(actualConfig.getRanksItem().getXPos(), actualConfig.getRanksItem().getYPos(),
+                 GuiFactory.displayableBuilder(ItemStack.class)
+                .itemStack(UtilConfigItem.fromConfigItem(actualConfig.getRanksItem()))
                 .clickHandler((envyPlayer, clickType) -> BetterDexRewardsUI.open((EnvyPlayer<EntityPlayerMP>) envyPlayer))
                 .build());
 
-        pane.set(5, 1, GuiFactory.displayableBuilder(ItemStack.class)
-                .itemStack(new ItemBuilder()
-                        .type(PixelmonItems.itemPixelmonSprite)
-                        .amount(1)
-                        .name(UtilChatColour.translateColourCodes('&', "&eMissing Pokemon"))
-                        .lore()
-                        .nbt("ndex", new NBTTagShort((short) EnumSpecies.Unown.getNationalPokedexInteger()))
-                        .build())
+        pane.set(actualConfig.getMissingItem().getXPos(), actualConfig.getMissingItem().getYPos(),
+                 GuiFactory.displayableBuilder(ItemStack.class)
+                .itemStack(UtilConfigItem.fromConfigItem(actualConfig.getMissingItem()))
                 .clickHandler((envyPlayer, clickType) -> DexRewardsMissingUI.open((EnvyPlayer<EntityPlayerMP>) envyPlayer))
                 .build());
 
-        ConfigItem infoItem = BetterDexRewards.getInstance().getConfig().getInfoItem();
+        PositionableConfigItem infoItem = BetterDexRewards.getInstance().getConfig().getInfoItem();
 
-        pane.set(7, 1, GuiFactory.displayableBuilder(ItemStack.class)
-                .itemStack(new ItemBuilder()
-                        .type(Item.getByNameOrId(infoItem.getType()))
-                        .amount(infoItem.getAmount())
-                        .name(UtilChatColour.translateColourCodes('&', infoItem.getName()))
-                        .lore(infoItem.getLore())
-                        .damage(infoItem.getDamage())
-                        .build())
+        pane.set(infoItem.getXPos(), infoItem.getYPos(), GuiFactory.displayableBuilder(ItemStack.class)
+                .itemStack(UtilConfigItem.fromConfigItem(infoItem))
                 .build());
 
         GuiFactory.guiBuilder()
                 .addPane(pane)
-                .setCloseConsumer(envyPlayer -> {
-                })
+                .setCloseConsumer(envyPlayer -> {})
                 .setPlayerManager(BetterDexRewards.getInstance().getPlayerManager())
                 .height(3)
                 .title(UtilChatColour.translateColourCodes('&', config.getTitle()))
                 .build().open(player);
+    }
+
+    private static List<String> getLore(double percentage, ConfigItem item) {
+        List<String> lore = Lists.newArrayList();
+
+        for (String s : item.getLore()) {
+            lore.add(UtilChatColour.translateColourCodes('&', s.replace(
+                    "%percentage%",
+                    String.format("%.2f", percentage) + "%"
+            )));
+        }
+
+        return lore;
     }
 }
