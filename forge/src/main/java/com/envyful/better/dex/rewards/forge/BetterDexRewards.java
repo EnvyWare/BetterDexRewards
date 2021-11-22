cdae7a0c077a004d3e12ef6c8920215335235b8b
Daniel Norris <dnorris443@gmail.com>
2021-11-22 16:31:30 +0000

    chore(gradle): version bump to 2.0.0 inline with Pixelmon sidemods

--- forge/src/main/java/com/envyful/better/dex/rewards/forge/BetterDexRewards.java
+++ forge/src/main/java/com/envyful/better/dex/rewards/forge/BetterDexRewards.java
@@ -33,7 +33,7 @@ import java.sql.SQLException;
 )
 public class BetterDexRewards {
 
-    protected static final String VERSION = "1.6.1";
+    protected static final String VERSION = "2.0.0";
 
     private static BetterDexRewards instance;
 
