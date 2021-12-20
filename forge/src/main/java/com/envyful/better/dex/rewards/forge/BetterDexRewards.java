a7ebc1005730caf1e2a9bfa181b99747bb4d4134
danorris709 <dnorris443@gmail.com>
2021-12-20 19:56:01 +0000

    chore(gradle): update version to 2.1.7

--- forge/src/main/java/com/envyful/better/dex/rewards/forge/BetterDexRewards.java
+++ forge/src/main/java/com/envyful/better/dex/rewards/forge/BetterDexRewards.java
@@ -36,7 +36,7 @@ import java.sql.SQLException;
 )
 public class BetterDexRewards {
 
-    protected static final String VERSION = "2.1.6";
+    protected static final String VERSION = "2.1.7";
 
     private static BetterDexRewards instance;
 
