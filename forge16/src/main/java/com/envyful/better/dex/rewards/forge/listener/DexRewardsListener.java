c11461c9f500d6dbe5054cd11ad7add34e0d2006
Daniel Norris <dnorris443@gmail.com>
2022-06-20 20:04:15 +0100

    feat(forge16): update listener, playerdata and task

---
+++ forge16/src/main/java/com/envyful/better/dex/rewards/forge/listener/DexRewardsListener.java
@@ -0,0 +1,63 @@
+package com.envyful.better.dex.rewards.forge.listener;
+
+import com.envyful.api.concurrency.UtilConcurrency;
+import com.envyful.api.forge.chat.UtilChatColour;
+import com.envyful.api.forge.player.ForgeEnvyPlayer;
+import com.envyful.api.forge.player.util.UtilPlayer;
+import com.envyful.better.dex.rewards.forge.BetterDexRewards;
+import com.envyful.better.dex.rewards.forge.config.BetterDexRewardsConfig;
+import com.envyful.better.dex.rewards.forge.player.DexRewardsAttribute;
+import com.pixelmonmod.pixelmon.Pixelmon;
+import com.pixelmonmod.pixelmon.api.events.PokedexEvent;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.util.Map;
+
+public class DexRewardsListener {
+
+    private final BetterDexRewards mod;
+
+    public DexRewardsListener(BetterDexRewards mod) {
+        this.mod = mod;
+
+        Pixelmon.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onPokemonCatch(PokedexEvent event) {
+        ServerPlayerEntity entityPlayerMP = UtilPlayer.getOnlinePlayer(event.uuid);
+
+        if (entityPlayerMP == null) {
+            return;
+        }
+
+        UtilConcurrency.runAsync(() -> {
+            ForgeEnvyPlayer player = this.mod.getPlayerManager().getPlayer(event.uuid);
+            DexRewardsAttribute attribute = player.getAttribute(BetterDexRewards.class);
+
+            if (attribute == null) {
+                return;
+            }
+
+            double percentage = attribute.getPokeDexPercentage();
+
+            for (Map.Entry<String, BetterDexRewardsConfig.DexCompletion> entry : this.mod.getConfig().getRewardStages().entrySet()) {
+                if (attribute.hasClaimed(entry.getKey())) {
+                    continue;
+                }
+
+                if (percentage < entry.getValue().getRequiredPercentage()) {
+                    continue;
+                }
+
+                for (String s : this.mod.getConfig().getClaimReminderMessage()) {
+                    player.message(UtilChatColour.translateColourCodes('&', s));
+                }
+
+                break;
+            }
+        });
+    }
+
+}
---
+++ forge16/src/main/java/com/envyful/better/dex/rewards/forge/task/ReminderTask.java
@@ -0,0 +1,58 @@
+package com.envyful.better.dex.rewards.forge.task;
+
+import com.envyful.api.forge.chat.UtilChatColour;
+import com.envyful.api.forge.player.ForgeEnvyPlayer;
+import com.envyful.better.dex.rewards.forge.BetterDexRewards;
+import com.envyful.better.dex.rewards.forge.config.BetterDexRewardsConfig;
+import com.envyful.better.dex.rewards.forge.player.DexRewardsAttribute;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class ReminderTask implements Runnable {
+
+    private final BetterDexRewards mod;
+
+    public ReminderTask(BetterDexRewards mod) {
+        this.mod = mod;
+    }
+
+    @Override
+    public void run() {
+        for (ForgeEnvyPlayer onlinePlayer : this.mod.getPlayerManager().getOnlinePlayers()) {
+            DexRewardsAttribute attribute = onlinePlayer.getAttribute(BetterDexRewards.class);
+
+            if (attribute == null) {
+                continue;
+            }
+
+            if ((System.currentTimeMillis() - attribute.getLastReminder()) <=
+                    TimeUnit.SECONDS.toMillis(this.mod.getConfig().getMessageDelaySeconds())) {
+                continue;
+            }
+
+            if (!this.canClaimReward(attribute)) {
+                continue;
+            }
+
+            attribute.setLastReminder(System.currentTimeMillis());
+
+            for (String s : this.mod.getConfig().getClaimReminderMessage()) {
+                onlinePlayer.message(UtilChatColour.translateColourCodes('&', s));
+            }
+        }
+    }
+
+    private boolean canClaimReward(DexRewardsAttribute attribute) {
+        double pokeDexPercentage = attribute.getPokeDexPercentage();
+
+        for (Map.Entry<String, BetterDexRewardsConfig.DexCompletion> entry :
+                this.mod.getConfig().getRewardStages().entrySet()) {
+            if (pokeDexPercentage >= entry.getValue().getRequiredPercentage() && !attribute.hasClaimed(entry.getKey())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
