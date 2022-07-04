9ae17637d2dfd386562a2061910380fc19070312
Daniel Norris <dnorris443@gmail.com>
2022-07-04 04:18:00 +0100

    chore(gradle): bump version to 3.0.6

--- forge/src/main/java/com/envyful/better/dex/rewards/forge/BetterDexRewards.java
+++ forge/src/main/java/com/envyful/better/dex/rewards/forge/BetterDexRewards.java
@@ -36,7 +36,7 @@ import java.sql.SQLException;
 )
 public class BetterDexRewards {
 
-    protected static final String VERSION = "3.0.5";
+    protected static final String VERSION = "3.0.6";
 
     private static BetterDexRewards instance;
 
