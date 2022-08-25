74b39d1ec72a2f6bc362668c5cd0a9184ab6d85d
Daniel Norris <dnorris443@gmail.com>
2022-08-25 17:14:03 +0100

    fix(forge16): prevent weird UI crash

--- forge/src/main/java/com/envyful/better/dex/rewards/forge/BetterDexRewards.java
+++ forge/src/main/java/com/envyful/better/dex/rewards/forge/BetterDexRewards.java
@@ -36,7 +36,7 @@ import java.sql.SQLException;
 )
 public class BetterDexRewards {
 
-    protected static final String VERSION = "3.0.8";
+    protected static final String VERSION = "3.1.0";
 
     private static BetterDexRewards instance;
 
