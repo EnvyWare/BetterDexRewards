60f2806686fe07f2e1460a272732641c0dd72156
Daniel Norris <dnorris443@gmail.com>
2023-05-28 15:30:23 +0100

    fix(transformer): use API not duplicate method for getting biome names

--- forge16/src/main/java/com/envyful/better/dex/rewards/forge/transformer/BiomesTransformer.java
+++ forge16/src/main/java/com/envyful/better/dex/rewards/forge/transformer/BiomesTransformer.java
@@ -1,18 +1,8 @@
 package com.envyful.better.dex.rewards.forge.transformer;
 
 import com.envyful.api.gui.Transformer;
-import com.google.common.collect.Lists;
+import com.envyful.api.reforged.pixelmon.UtilPokemonInfo;
 import com.pixelmonmod.pixelmon.api.pokemon.species.Stats;
-import com.pixelmonmod.pixelmon.api.spawning.SpawnInfo;
-import com.pixelmonmod.pixelmon.api.spawning.SpawnSet;
-import com.pixelmonmod.pixelmon.api.spawning.archetypes.entities.pokemon.SpawnInfoPokemon;
-import com.pixelmonmod.pixelmon.api.util.helpers.BiomeHelper;
-import com.pixelmonmod.pixelmon.spawning.PixelmonSpawning;
-import net.minecraft.util.ResourceLocation;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class BiomesTransformer implements Transformer {
 
@@ -28,30 +18,6 @@ public class BiomesTransformer implements Transformer {
 
     @Override
     public String transformName(String name) {
-        return name.replace("%biomes%", String.join("§7,§f ", getSpawnBiomes(this.pokemon)));
-    }
-
-    public static List<String> getSpawnBiomes(Stats pokemon) {
-        List<String> names = Lists.newArrayList();
-
-        for (SpawnSet next : PixelmonSpawning.getAll().values().stream().flatMap(Collection::stream).collect(Collectors.toList())) {
-            for (SpawnInfo spawnInfo : next.spawnInfos) {
-                if (!(spawnInfo instanceof SpawnInfoPokemon)) {
-                    continue;
-                }
-
-                SpawnInfoPokemon spawnInfoPokemon = (SpawnInfoPokemon)spawnInfo;
-
-                if (!spawnInfoPokemon.getSpecies().equals(pokemon.getParentSpecies()) || spawnInfoPokemon.spawnSpecificBossRate != null) {
-                    continue;
-                }
-
-                for (ResourceLocation biome : spawnInfoPokemon.condition.biomes) {
-                    names.add(BiomeHelper.getLocalizedBiomeName(biome).getString());
-                }
-            }
-        }
-
-        return names;
+        return name.replace("%biomes%", String.join("§7,§f ", UtilPokemonInfo.getSpawnBiomes(this.pokemon)));
     }
 }
